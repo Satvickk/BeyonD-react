89ce2458ce5ea37c559fe7f819c9d3f9427137b8
Satvickk <lenovo37819@gmail.com>
2023-11-20 21:05:33 +0530

    second commit

---
+++ src/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './Components/Footer/Footer'
+
+
+function Layout() {
+  return (
+    <>
+    <Header />
+
+    <Outlet />    {/* outlet beech me dene se hum header aur footer ko same rakhte hue outlet ki jagah alag alag component insert kr sakte hai */}
+    
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
