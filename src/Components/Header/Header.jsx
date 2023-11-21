50dc986797e7e2a01d6e82518184ff1899d55f7e
Satvickk <lenovo37819@gmail.com>
2023-11-21 22:28:25 +0530

    commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,3 @@
-// we use Link in place of <a> tag because a tag refreshes/reloads the whole page but react does not have a concept i.e it has virtual dom concept  (Syntax is changed)
-
-
 import React from 'react'
 import {Link, NavLink} from 'react-router-dom'
 
@@ -37,10 +34,9 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    to='/'
+                                to="/"
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
-                                        ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
@@ -48,10 +44,9 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                    to='/About'
+                                to="/about"
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
-                                        ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     About
@@ -59,16 +54,24 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                    to='/Contact'
+                                to="/contact"
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
-                                        ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Contact
                                 </NavLink>
                             </li>
-                            
+                            <li>
+                                <NavLink
+                                to="/github"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Github
+                                </NavLink>
+                            </li>
                             
                         </ul>
                     </div>
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import Header from './Components/Header/Header'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
-import Footer from './Components/Footer/Footer'
-
 
 function Layout() {
   return (
     <>
-    <Header />
-
-    <Outlet />    {/* outlet beech me dene se hum header aur footer ko same rakhte hue outlet ki jagah alag alag component insert kr sakte hai */}
-    
+    <Header/>
+    <Outlet />
     <Footer />
     </>
   )
